9a9663ed77086d36354fe330900983c91063cef4
X-Cyborg <puneethkshetty6720@gmail.com>
2023-05-08 11:29:16 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -3,24 +3,44 @@
 #include "Receiver.h"
 #include "Sender.h"
 
-void function1(){
-    printf("Display Input1\n");
+void function1(int input){
+    printf("Display function 1 :%d\n",input);
 }
 
-void function2(){
-    printf("Display Input2\n");
+void function2(int input){
+    printf("Display function 2 :%d\n",input);
 }
 
-void function3(){
-    printf("Display Input3\n");
+void function3(int input){
+    printf("Display function 3 :%d\n",input);
 }
 
 void register_function(callback c)
 {   
-   for(int i=0;i<num_inputs;i++)
-   {
-      register_callback(i,c);
-   }
+//     // int random;
+//     // // callback funcs[] = {&function1,&function2,&function3};
+//     // scanf("%d",&random);
+//     // switch (random % 3)
+//     // {
+//     // case 0:
+//     //     c = function1;
+//     //     break;
+//     // case 1:
+//     //     c = function2;
+//     //     break;
+//     // case 2:
+//     //     c = function3;
+//     //     break;
+//     // default:
+//     //     printf("No function to call");
+//     //     break;
+//     // }
+
+//     // c(1);
+    for(int i=0;i< num_inputs;i++)
+    {
+        register_callback(i,c);
+    }
 
 }
 
